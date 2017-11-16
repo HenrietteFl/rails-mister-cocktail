01b0a205aaeb7ffad749cc096eb0bc0200dd2a52
Henriette <henriette.fleischmann@gmail.com>
2017-11-16 13:10:00 +0100

    controllers and routes set up

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,25 @@
+class DosesController < ApplicationController
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.ingredient = Ingredient.find(params[:ingredient_id])
+
+    if @dose.save
+      redirect_to @dose.cocktail
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :amount)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+require "json"
+
+puts "Cleaning db..."
+Ingredient.destroy_all
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+serialized_cocktails = File.read(url)
+cocktails = JSON.parse(serialized_cocktails)
+
+cocktails['drinks'].each do |cocktail|
+  Ingredient.create(name: cocktail['strIngredient1'])
+end
